c36a1ece032218b752551c2aafb67c85cc1e00df
pecodigos <pedrenriquecunha@gmail.com>
2024-08-03 06:07:12 -0300

    Service layer and Component registration

--- src/main/java/com/contratae/project/config/TestConfig.java
+++ src/main/java/com/contratae/project/config/TestConfig.java
@@ -16,7 +16,6 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
-
     @Override
     public void run(String... args) throws Exception {
 
